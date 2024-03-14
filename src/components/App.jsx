a0d4adaf68b28092ed82a5904afcccdd21a039c0
VorobeiIvan <0675361905vi@gmail.com>
2024-03-14 20:23:46 +0200

    fix thunk and app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
+import React, { useEffect } from 'react';
 import Section from './Section';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 import './main.css';
+import { useDispatch } from 'react-redux';
+import { fetchContactsThunk } from '../store/thunk';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContactsThunk());
+  }, [dispatch]);
+
   return (
     <div className="container">
       <Section title="Phonebook">
--- src/store/thunk.js
+++ src/store/thunk.js
@@ -1,23 +1,44 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { fetchContacts, addContact, deleteContact } from './api';
+import { fetchContacts, addContact, deleteContact } from '../store/api';
 
 export const fetchContactsThunk = createAsyncThunk(
   'contacts/fetchAll',
   async () => {
-    return await fetchContacts();
+    try {
+      const data = await fetchContacts();
+      console.log('Fetched contacts:', data);
+      return data;
+    } catch (error) {
+      console.error('Error fetching contacts:', error);
+      throw error;
+    }
   }
 );
 
 export const addContactThunk = createAsyncThunk(
   'contacts/addContact',
   async contactData => {
-    return await addContact(contactData);
+    try {
+      const data = await addContact(contactData);
+      console.log('Added contact:', data);
+      return data;
+    } catch (error) {
+      console.error('Error adding contact:', error);
+      throw error;
+    }
   }
 );
 
 export const deleteContactThunk = createAsyncThunk(
   'contacts/deleteContact',
   async contactId => {
-    return await deleteContact(contactId);
+    try {
+      await deleteContact(contactId);
+      console.log('Deleted contact with ID:', contactId);
+      return contactId;
+    } catch (error) {
+      console.error('Error deleting contact:', error);
+      throw error;
+    }
   }
 );
