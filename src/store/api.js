d02aa645acd238b5c7debb371b145a61ebdacaa5
VorobeiIvan <0675361905vi@gmail.com>
2024-03-14 21:57:34 +0200

    fix app

--- src/store/api.js
+++ src/store/api.js
@@ -4,40 +4,26 @@ const baseURL = 'https://65f1f4bc034bdbecc7641d5b.mockapi.io/contacts/';
 const contactsEndpoint = '/contacts';
 
 export const fetchContacts = async () => {
-  try {
-    const response = await axios.get(`${baseURL}${contactsEndpoint}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching contacts:', error);
-    throw new Error('Failed to fetch contacts');
-  }
+  const response = await axios.get(`${baseURL}${contactsEndpoint}`);
+  return response.data;
 };
 
 export const addContact = async contactData => {
-  try {
-    const response = await axios.post(`${baseURL}${contactsEndpoint}`, contactData);
-    return response.data;
-  } catch (error) {
-    console.error('Error adding contact:', error);
-    throw new Error('Failed to add contact');
-  }
+  const response = await axios.post(
+    `${baseURL}${contactsEndpoint}`,
+    contactData
+  );
+  return response.data;
 };
+
 export const deleteContact = async contactId => {
-  try {
-    await axios.delete(`${baseURL}${contactsEndpoint}/${contactId}`);
-    return contactId;
-  } catch (error) {
-    console.error('Error deleting contact:', error);
-    throw new Error('Failed to delete contact');
-  }
+  await axios.delete(`${baseURL}${contactsEndpoint}/${contactId}`);
+  return contactId;
 };
 
 export const filterContacts = async filter => {
-  try {
-    const response = await axios.get(`${baseURL}${contactsEndpoint}?filter=${filter}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error filtering contacts:', error);
-    throw new Error('Failed to filter contacts');
-  }
+  const response = await axios.get(
+    `${baseURL}${contactsEndpoint}?filter=${filter}`
+  );
+  return response.data;
 };
